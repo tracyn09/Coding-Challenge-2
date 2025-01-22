23acd4f9c5a279c2097e2ea81c139b0797041e8f
tracyn09 <tracyn0099@gmail.com>
2025-01-21 20:46:41 -0500

    Task 3– Working with Objects

--- cc_2.js
+++ cc_2.js
@@ -25,3 +25,11 @@ let averageScore = scores.reduce((sum, num) => sum + num + 0)/ scores.length;
 // Logging Updated Array and Average Score
 console.log("Scores:", scores);
 console.log("Average Score: ", averageScore); 
+
+// Task 3
+let employee = ["Joseph", "19", "Finance"]
+var isActive = true
+employee[2] = "Accounting" // Updating Dept
+employee.push("Auditor") // Adding New Property
+console.log(employee);
+
