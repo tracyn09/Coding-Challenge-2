c8adad5be473e4038c861240339fb2ae82541c12
tracyn09 <tracyn0099@gmail.com>
2025-01-21 20:09:52 -0500

    Task 1– Working with Arrays

---
+++ cc_2.js
@@ -0,0 +1,22 @@
+//Task 1
+let products = ["MacBook Pro", "Airpods", "Airpod Max", "IPad Pro", "Apple Pencil"];
+console.log(products);
+
+ // Adding New Product
+products.push("IPhone 16");
+console.log(products);
+
+// Removing Last Product Listed
+let removed = products.pop();
+console.log(removed);
+
+//Updated Product List
+console.log(products);
+
+//Task 2
+
+//Task 3
+
+//Task 4
+
+//Task 5
